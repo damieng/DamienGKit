b185354bd423f51667e52375f7904d2c67ef22f6
Damien Guard <damien@envytech.co.uk>
2013-08-18 22:15:02 -0700

    Correct file name for EquatableWeakReference.

--- CSharp/DamienG.Library/System/EquatableWeakReference.cs
+++ CSharp/DamienG.Library/System/EquatableWeakReference.cs
@@ -1,3 +1,8 @@
+// Copyright (c) Damien Guard.  All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Originally published at http://damieng.com/blog/2006/08/28/equatable_weak_references
+
 using System;
 using System.Runtime.InteropServices;
 
