78839b5494e9516c33d70c02e67b2cf05428149f
Damien Guard <damieng@gmail.com>
2022-12-06 11:55:21 +0000

    Update Crc32.cs

--- CSharp/DamienG.Library/Security/Cryptography/Crc32.cs
+++ CSharp/DamienG.Library/Security/Cryptography/Crc32.cs
@@ -20,7 +20,7 @@ namespace DamienG.Security.Cryptography
     /// </remarks>
     public sealed class Crc32 : HashAlgorithm
     {
-        public const UInt32 DefaultPolynomial = 0x04C11DB7u;
+        public const UInt32 DefaultPolynomial = 0xedb88320u;
         public const UInt32 DefaultSeed = 0xffffffffu;
 
         static UInt32[] defaultTable;
@@ -28,9 +28,6 @@ namespace DamienG.Security.Cryptography
         readonly UInt32 seed;
         readonly UInt32[] table;
         UInt32 hash;
-        UInt32 xorOut = 0xffffffff;
-        bool reflectIn = true;
-        bool reflectOut = true;
 
         /// <summary>
         /// Create a new <see cref="Crc32"/> with a <see cref="DefaultPolynomial"/> and <see cref="DefaultSeed"/>.
@@ -41,22 +38,17 @@ namespace DamienG.Security.Cryptography
         }
 
         /// <summary>
-        /// Create a new <see cref="Crc32"/> with a supplied polynomial, seed, and algorithm options..
-        /// Note: the "default" CRC32 algorithm reflects both input and output, then flips all bits (XorOut = 0xFFFFFFFF). 
+        /// Create a new <see cref="Crc32"/> with a supplied polynomial and see.
         /// </summary>
         /// <param name="polynomial">The polynomial to use in calculating.</param>
         /// <param name="polynomial">The initial seed to start from.</param>
-        public Crc32(UInt32 polynomial, UInt32 seed, UInt32 XorOut = 0xFFFFFFFF, bool refIn = true, bool refOut = true)
+        public Crc32(UInt32 polynomial, UInt32 seed)
         {
             if (!BitConverter.IsLittleEndian)
                 throw new PlatformNotSupportedException("Not supported on Big Endian processors");
 
-            table = InitializeTable(polynomial, refIn);
+            table = InitializeTable(polynomial);
             this.seed = hash = seed;
-
-            this.xorOut = XorOut;
-            this.reflectIn = refIn;
-            this.reflectOut = refOut;
         }
 
         /// <inheritdoc/>
@@ -68,13 +60,13 @@ namespace DamienG.Security.Cryptography
         /// <inheritdoc/>
         protected override void HashCore(byte[] array, int ibStart, int cbSize)
         {
-            hash = CalculateHash(table, hash, array, ibStart, cbSize, xorOut, reflectIn, reflectOut);
+            hash = CalculateHash(table, hash, array, ibStart, cbSize);
         }
 
         /// <inheritdoc/>
         protected override byte[] HashFinal()
         {
-            var hashBuffer = UInt32ToBigEndianBytes(hash ^ xorOut);
+            var hashBuffer = UInt32ToBigEndianBytes(~hash);
             HashValue = hashBuffer;
             return hashBuffer;
         }
@@ -114,36 +106,24 @@ namespace DamienG.Security.Cryptography
         /// </summary>
         /// <param name="polynomial">The polynomial to calculate a table for.</param>
         /// <returns>A <see cref="UInt32[]"/> table to be used in calculating a CRC32.</returns>
-        static UInt32[] InitializeTable(UInt32 polynomial, bool refIn = true)
+        static UInt32[] InitializeTable(UInt32 polynomial)
         {
-            if (polynomial == DefaultPolynomial && defaultTable != null && refIn)
+            if (polynomial == DefaultPolynomial && defaultTable != null)
                 return defaultTable;
 
-            uint bit; 
             var createTable = new UInt32[256];
             for (var i = 0; i < 256; i++)
             {
-                var entry = (UInt32)(refIn ? reflect((uint)i, 8) : (uint)i);
-
-                entry <<= 24;
-                for(var j =0; j < 8; j++)
-                {
-                    bit = entry & (1u << 31);
-                    entry <<= 1;
-                    if (bit > 0)
-                    {
-                        entry ^= polynomial;
-                    }
-                }
-
-                if (refIn)
-                {
-                    entry = reflect(entry, 32); 
-                }
+                var entry = (UInt32)i;
+                for (var j = 0; j < 8; j++)
+                    if ((entry & 1) == 1)
+                        entry = (entry >> 1) ^ polynomial;
+                    else
+                        entry >>= 1;
                 createTable[i] = entry;
             }
 
-            if (polynomial == DefaultPolynomial && refIn)
+            if (polynomial == DefaultPolynomial)
                 defaultTable = createTable;
 
             return createTable;
@@ -159,27 +139,11 @@ namespace DamienG.Security.Cryptography
         /// <param name="size">How many bytes within the <paramref name="buffer"/> to read in calculating the CRC32.</param>
         /// <returns>The bit-inverted CRC32.</returns>
         /// <remarks>This hash is bit-inverted. Use other methods in this class or <see langword="~"/> the result from this method.</remarks>
-        static UInt32 CalculateHash(UInt32[] table, UInt32 seed, IList<byte> buffer, int start, int size, uint xorOut = 0xffffffff, bool refIn = true, bool refOut = true)
+        static UInt32 CalculateHash(UInt32[] table, UInt32 seed, IList<byte> buffer, int start, int size)
         {
             var hash = seed;
-            //if (refIn) hash = reflect(hash, 32); 
-
-            if (refIn)
-            {
             for (var i = start; i < start + size; i++)
                 hash = (hash >> 8) ^ table[buffer[i] ^ hash & 0xff];
-            }
-            else
-            {
-                for (var i = start; i < start + size; i++)
-                    hash = (hash << 8) ^ table[buffer[i] ^ ((hash >> 24) & 0xff)];
-            }
-
-            if (refIn ^ refOut)
-            {
-                hash = reflect(hash, 32); 
-            }
-
             return hash;
         }
 
@@ -198,28 +162,5 @@ namespace DamienG.Security.Cryptography
 
             return result;
         }
-
-        /// <summary>
-        /// Helper to support bitwise reflecting (reversing) of bytes.
-        /// </summary>
-        /// <param name="crc"></param>
-        /// <param name="bitnum"></param>
-        /// <returns></returns>
-        public static UInt32 reflect(UInt32 crc, int bitnum)
-        {
-
-            // reflects the lower 'bitnum' bits of 'crc'
-
-            UInt32 i, j = 1, crcout = 0;
-
-            for (i = (UInt32)1 << (bitnum - 1); i > 0; i >>= 1)
-            {
-                if ((crc & i) > 0)
-                    crcout |= j;
-                j <<= 1;
-            }
-            return (crcout);
-        }
-
     }
 }
